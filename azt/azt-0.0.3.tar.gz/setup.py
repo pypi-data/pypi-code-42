44c8831f754fc1f83075f868f1dbdfff25deabe7
Tom Forbes <tom@tomforb.es>
2019-09-20 14:44:52 +0000

    {"name":"azt","version":"0.0.3","file":"azt-0.0.3.tar.gz","path":"azt/azt-0.0.3.tar.gz"}

---
+++ azt/azt-0.0.3.tar.gz/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='azt',
+    version='0.0.3',
+    py_modules=['azt'],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'pyfiglet',
+    ],
+    entry_points='''
+        [console_scripts]
+        azt=azt:cli
+    ''',
+)
